abf3483507532d5ccc4d6c30e8aa91456074005a
loumcnamee <mcnamee.lou@gmail.com>
2024-10-19 10:34:57 -0400

    Add Kahan summation to compensate for numeric errors

--- aLib/Strategy.cpp
+++ aLib/Strategy.cpp
@@ -4,6 +4,44 @@ Strategy::Strategy() : _name("none") {} //default constructor
 
 Strategy::Strategy(std::string name) : _name(name) {} // constructor assigning name
 
+const std::string& Strategy::name() const {
+    return _name;
+}
+
+float Strategy::kahanSum(const std::vector<float>& data) {
+    
+    // Prepare the accumulator.
+    float sum = 0.0f;
+    
+    // A running compensation for lost low-order bits.
+    float c = 0.0f;
+
+    float y = 0.0f;
+    float t = 0.0f;
+
+    // The array input has elements indexed input[1] to input[input.length].
+    for (auto i : data ) {
+        
+        // c is zero the first time around.
+        y = i - c;
+        
+        // Alas, sum is big, y small, so low-order digits of y are lost.         
+        t = sum + y;
+        
+        // (t - sum) cancels the high-order part of y;
+        // subtracting y recovers negative (low part of y)
+        
+        c = (t - sum) - y;
+        // Algebraically, c should always be zero. Beware
+        // overly-aggressive optimizing compilers!
+        
+        sum = t;
+        // Next time around, the lost low part will be added to y in a fresh attempt.
+    }
+   
+    return sum;
+}
+
 
 
 
--- aLib/Strategy.h
+++ aLib/Strategy.h
@@ -15,11 +15,22 @@ class Strategy
 {
 private:
   std::string _name;
-
-public:
   Strategy();
+  
+public:
+  
   Strategy(std::string name); // constructor assigning name
+  const std::string& name() const;
   virtual ~Strategy() = default;
+  
+  // api to calculate a mean from a vector of floats
+  // derived classes must override this member fucntion
   virtual float mean(std::vector<float>) = 0;
+
+  // the Kahan summation algorithm, also known as compensated summation
+  // significantly reduces the numerical error in the total obtained by
+  // adding a sequence of finite-precision floating-point numbers
+  // source https://en.wikipedia.org/wiki/Kahan_summation_algorithm
+  float kahanSum(const std::vector<float>& data);
 };
 
